57315122b23a051a4a88bd544879543195e97708
Eliott Teissonniere <gitlab.eliott@teissonniere.org>
2018-08-23 20:24:20 +0200

    Add one more test

--- test/safe.js
+++ test/safe.js
@@ -2,6 +2,7 @@ contract('Safe', async accounts => {
     it('deploy new safe from factory')
     it('deposits some funds')
     it('adds new modules')
+    it('cannot add twice');
     it('removes modules')
     it('can not remove last verification module')
     it('successfully executes transaction')
