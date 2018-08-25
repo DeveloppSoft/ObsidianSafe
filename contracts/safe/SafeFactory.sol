f96bc312c9f58a46c46f6415086a678adfd62025
Eliott Teissonniere <gitlab.eliott@teissonniere.org>
2018-08-25 11:15:49 +0200

    Factory

--- contracts/safe/SafeFactory.sol
+++ contracts/safe/SafeFactory.sol
@@ -1,7 +1,19 @@
 pragma solidity ^0.4.22;
 
 
+import './Safe.sol';
+
+// TODO delegate proxy?
+
 contract SafeFactory {
-  constructor() {
-  }
+    event SafeCreated(address indexed _sender, address _safe);
+
+    // TODO configure initial modules
+    function createSafe(address _initialVerifModule, address _initialExecModule) returns (address) {
+        Safe safe = new Safe(_initialVerifModule, _initialExecModule);
+
+        SafeCreated(msg.sender, address(safe));
+
+        return address(safe);
+    }
 }
