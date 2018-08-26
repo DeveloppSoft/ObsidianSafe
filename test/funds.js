63039835ed58182e47b7e0e04f75d05b8e2cd1cc
Eliott Teissonniere <gitlab.eliott@teissonniere.org>
2018-08-26 13:05:14 +0200

    No need for fund module

--- test/funds.js
+++
@@ -1,7 +0,0 @@
-contract('Funds', async accounts => {
-    it('deploys and configure module')
-    it('deposits ETH')
-    it('deposits tokens')
-    it('withdraw ETH')
-    it('withdraw tokens')
-})
