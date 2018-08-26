e7cb3d818e035f0a03fe09147a4332604a3183f0
Eliott Teissonniere <gitlab.eliott@teissonniere.org>
2018-08-26 14:51:37 +0200

    Also test installed modules

--- test/safe_factory.js
+++ test/safe_factory.js
@@ -1,8 +1,10 @@
 const SafeFactory = artifacts.require('SafeFactory')
 const Safe = artifacts.require('Safe')
+const Signers = artifacts.require('Signers')
 
 contract('SafeFactory', async accounts => {
     let factory = {}
+    let safe = {}
 
     it('should have deployed factory', async () => {
         factory = await SafeFactory.deployed()
@@ -13,9 +15,18 @@ contract('SafeFactory', async accounts => {
         const receipt = await factory.createSafe(accounts[1])
         const safeAddress = receipt.logs[0].args._safe
 
-        const safe = await Safe.at(safeAddress)
-        assert.isTrue(await safe.isNonceValid(1), "Contract is not a new safe")
+        safe = await Safe.at(safeAddress)
+        assert.isTrue(await safe.isNonceValid(1), 'Contract is not a new safe')
     })
 
-    // Shall we check the modules installed?
+    it('signer correctly configured', async () => {
+        const modules = await safe.listVerifModules()
+        const signerAddress = modules[0] // Only 1 module
+        const signerModule = await Signers.at(signerAddress)
+        const signers = await signerModule.listSigners()
+
+        assert.equal(signers.length, 1, 'Must have only one signer')
+        assert.equal(signers[0], accounts[1], 'Address mismatch')
+        assert.isTrue(await signerModule.isSigner(accounts[1]), 'Account should be a signer')
+    })
 })
