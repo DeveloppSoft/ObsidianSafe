18cd94c0d841d250b501698f692ac38789bcc6d5
Eliott Teissonniere <gitlab.eliott@teissonniere.org>
2018-09-02 17:56:31 +0200

    Relink contracts

--- migrations/2_libraries.js
+++ migrations/2_libraries.js
@@ -1,9 +1,9 @@
 const ListOnSteroids = artifacts.require('ListOnSteroids')
-const Signers = artifacts.require('Signers')
+const AuthOracle = artifacts.require('AuthOracle')
 const SafeFactory = artifacts.require('SafeFactory')
 
 module.exports = async deployer => {
     deployer.deploy(ListOnSteroids)
-    deployer.link(ListOnSteroids, Signers)
+    deployer.link(ListOnSteroids, AuthOracle)
     deployer.link(ListOnSteroids, SafeFactory)
 }
